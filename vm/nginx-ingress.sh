2db4d6185cf20ed76d63d7b93b4f5b7f06ae76ef
Henryarrovin <henry.a.s.arrovin@gmail.com>
2026-05-10 05:38:37 +0530

    added nginx ingress install script

---
+++ vm/nginx-ingress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing nginx ingress..."
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/cloud/deploy.yaml
+
+kubectl wait --namespace ingress-nginx \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=120s
