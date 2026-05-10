03b7902f779cfcf2a58978acf48d3f0ac5c061f0
Henryarrovin <henry.a.s.arrovin@gmail.com>
2026-05-11 00:11:43 +0530

    added sudo prev in cmd

--- vm/configure-kubectl.sh
+++ vm/configure-kubectl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy kubeconfig from master to server container
-mkdir -p /root/.kube
+sudo mkdir -p /root/.kube
 multipass exec master -- bash -c "sudo cat /etc/rancher/k3s/k3s.yaml" > /root/.kube/config
 
 MASTER_IP=$(multipass info master | grep IPv4 | awk '{print $2}')
@@ -9,7 +9,7 @@ echo "Master IP: $MASTER_IP"
 
 # kubeconfig has 127.0.0.1 (master's localhost)
 # We need the actual master IP
-sed -i "s/127.0.0.1/${MASTER_IP}/" /root/.kube/config
+sudo sed -i "s/127.0.0.1/${MASTER_IP}/" /root/.kube/config
 
 echo "Waiting for all nodes to be ready..."
 kubectl wait --for=condition=ready node --all --timeout=120s
